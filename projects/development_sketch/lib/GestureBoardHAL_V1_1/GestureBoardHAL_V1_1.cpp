8b5929f362efda8238569095ab23a0761978bc58
Arnaud PHELIPOT <aphelipot@gmail.com>
2016-12-06 23:06:23 +0100

    Ajoutées fonctions d'abstraction init et set (LED).

--- projects/development_sketch/lib/GestureBoardHAL_V1_1/GestureBoardHAL_V1_1.cpp
+++ projects/development_sketch/lib/GestureBoardHAL_V1_1/GestureBoardHAL_V1_1.cpp
@@ -1,48 +1,75 @@
 #include "GestureBoardHAL_V1_1.h"
 
-GESTURE_BOARD_HAL::GESTURE_BOARD_HAL()
+
+GestureBoardHalLedV1_1::GestureBoardHalLedV1_1(Animation* defaultAnim, int delay):GestureBoardHalLed(defaultAnim)
 {
-  pinMode(CHIP_ENABLE_2_8V, OUTPUT);
-  pinMode(GPIO_LED_RESET, OUTPUT);
-  pinMode(GPIO_RANGE_SENSORS_RESET,OUTPUT);
-  pinMode(LED_PWM_PIN, OUTPUT);
+  _pGPIO = new GPIO_EXPANDER(GPIO_EXPANDER_LED_ADDRESS);
+  _lastMillis = millis();
+  _delay = delay;
 }
 
-void GESTURE_BOARD_HAL::init()
+void GestureBoardHalLedV1_1::init()
 {
-  digitalWrite(CHIP_ENABLE_2_8V, 1);
+  _pGPIO->writeConfigurationPortPair(0x0000);
   delay(1000);
-
-  digitalWrite(GPIO_LED_RESET, 1);
-  digitalWrite(GPIO_RANGE_SENSORS_RESET, 1);
-
-  Wire.begin();
-  Serial.println("I2C link started");
-
-  Serial.begin(115200);
-  Serial.println("serial link started");
+  _pGPIO->writeRegisterPair(0x02, 0x0000);
+  delay(10);
 }
 
-void GESTURE_BOARD_HAL::assignSensorsAddress(GPIO_EXPANDER& sensorExpander, RANGE_SENSOR& sensorDefault, uint8_t address1, uint8_t address2, uint8_t address3, uint8_t address4, uint8_t address5)
+void GestureBoardHalLedV1_1::dim(int value)
 {
-  sensorExpander.writeRegister(GPIO_OUTPUT_REGISTER_PORT0, 0x01);
-  delay(10);
-  sensorDefault.writeByte(I2C_SLAVE__DEVICE_ADDRESS, address1);
-  Serial.println("1st address changed");
-  sensorExpander.writeRegister(GPIO_OUTPUT_REGISTER_PORT0, 0x03);
-  delay(10);
-  sensorDefault.writeByte(I2C_SLAVE__DEVICE_ADDRESS, address2);
-  Serial.println("2nd address changed");
-  sensorExpander.writeRegister(GPIO_OUTPUT_REGISTER_PORT0, 0x07);
-  delay(10);
-  sensorDefault.writeByte(I2C_SLAVE__DEVICE_ADDRESS, address3);
-  Serial.println("3rd address changed");
-  sensorExpander.writeRegister(GPIO_OUTPUT_REGISTER_PORT0, 0x0F);
-  delay(10);
-  sensorDefault.writeByte(I2C_SLAVE__DEVICE_ADDRESS, address4);
-  Serial.println("4th address changed");
-  sensorExpander.writeRegister(GPIO_OUTPUT_REGISTER_PORT0, 0x1F);
-  delay(10);
-  sensorDefault.writeByte(I2C_SLAVE__DEVICE_ADDRESS, address5);
-  Serial.println("5th address changed");
+
+}
+
+void GestureBoardHalLedV1_1::set(int mapping[])
+{
+  uint16_t physicalMapping = 0x0000;
+  physicalMapping = physicalMapping + mapping[13];
+  physicalMapping << 1;
+  physicalMapping = physicalMapping + mapping[0];
+  physicalMapping << 1;
+  physicalMapping = physicalMapping + mapping[2];
+  physicalMapping << 1;
+  physicalMapping = physicalMapping + mapping[1];
+  physicalMapping << 1;
+  physicalMapping = physicalMapping + mapping[3];
+  physicalMapping << 1;
+  physicalMapping = physicalMapping + mapping[5];
+  physicalMapping << 1;
+  physicalMapping = physicalMapping + mapping[4];
+  physicalMapping << 1;
+  physicalMapping << 1;
+
+  physicalMapping = physicalMapping + mapping[14];
+  physicalMapping << 1;
+  physicalMapping = physicalMapping + mapping[12];
+  physicalMapping << 1;
+  physicalMapping = physicalMapping + mapping[10];
+  physicalMapping << 1;
+  physicalMapping = physicalMapping + mapping[11];
+  physicalMapping << 1;
+  physicalMapping = physicalMapping + mapping[9];
+  physicalMapping << 1;
+  physicalMapping = physicalMapping + mapping[7];
+  physicalMapping << 1;
+  physicalMapping = physicalMapping + mapping[8];
+  physicalMapping << 1;
+  physicalMapping = physicalMapping + mapping[6];
+  physicalMapping << 1;
+
+  _pGPIO->writeRegisterPair(02, physicalMapping);
+}
+
+void GestureBoardHalLedV1_1::update()
+{
+  int newTick = millis();
+  if (this->_runningState)
+  {
+      if(newTick - this->_lastMillis >= this->_delay)
+      {
+          this->_count++;
+          this->_lastMillis = newTick;
+      }
+      this->_runningAnimation->update(*this, this->_count);
+  }
 }
--- projects/development_sketch/lib/GestureBoardHAL_V1_1/GestureBoardHAL_V1_1.h
+++ projects/development_sketch/lib/GestureBoardHAL_V1_1/GestureBoardHAL_V1_1.h
@@ -1,15 +1,20 @@
 
-#ifndef GESTURE_BOARD_HAL_H
-#define GESTURE_BOARD_HAL_H
+#ifndef GESTURE_BOARD_HAL_V1_1_H
+#define GESTURE_BOARD_HAL_V1_1_H
 
+#include "Arduino.h"
 #include "Wire.h"
 #include "GPIOExpanderPCAL6416A.h"
+#include "gestureBoardHAL_LED.h"
 #include "RangeSensorVL6180X.h"
 
 #define GPIO_LED_RESET 4
 #define GPIO_RANGE_SENSORS_RESET 5
 #define LED_PWM_PIN 6
 #define CHIP_ENABLE_2_8V 7
+
+#define GPIO_EXPANDER_LED_ADDRESS 0x20
+#define GPIO_EXPANDER_SENSORS_ADDRESS 0x21
 
 #define SENSOR_0_ADDRESS 0x29
 #define SENSOR_1_ADDRESS 0x31
@@ -18,11 +23,28 @@
 #define SENSOR_4_ADDRESS 0x34
 #define SENSOR_5_ADDRESS 0x35
 
-class GESTURE_BOARD_HAL
-{ public:
-  GESTURE_BOARD_HAL();
-  void init();
-  void assignSensorsAddress(GPIO_EXPANDER& sensorExpander, RANGE_SENSOR& sensorDefault, uint8_t address1, uint8_t address2, uint8_t address3, uint8_t address4, uint8_t address5);
+class GestureBoardHal_V1_1
+{
+public:
+  GestureBoardHal_V1_1();
+  //void init();
+  //void assignSensorsAddress(GPIO_EXPANDER& sensorExpander, RANGE_SENSOR& sensorDefault, uint8_t address1, uint8_t address2, uint8_t address3, uint8_t address4, uint8_t address5);
+};
+
+class GestureBoardHalLedV1_1 : public GestureBoardHalLed
+{
+
+private:
+  GPIO_EXPANDER* _pGPIO;
+  int _lastMillis;
+  int _delay;
+
+public:
+  GestureBoardHalLedV1_1(Animation* defaultAnim, int delay);
+  virtual void init();
+  virtual void dim(int value);
+  virtual void set(int mapping[]);
+  virtual void update();
 };
 
 
