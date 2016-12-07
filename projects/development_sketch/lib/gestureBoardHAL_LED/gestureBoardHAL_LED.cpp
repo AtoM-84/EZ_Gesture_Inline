7bcc8fb9984b79e522f513aa5d1881140b69f294
Arnaud PHELIPOT <aphelipot@gmail.com>
2016-12-07 14:44:51 +0100

    Portage librairie GestureBoardLED vers GestureBoardHAL_LED

--- projects/development_sketch/lib/gestureBoardHAL_LED/gestureBoardHAL_LED.cpp
+++ projects/development_sketch/lib/gestureBoardHAL_LED/gestureBoardHAL_LED.cpp
@@ -1,10 +1,10 @@
-
 #include "gestureBoardHAL_LED.h"
 
 GestureBoardHalLed::GestureBoardHalLed(Animation* defaultAnim):_runningAnimation(defaultAnim)
 {
   this->_count = 0;
   this->_runningState = false;
+  this->_map.mappingLed_byte = 0;
 }
 
 void GestureBoardHalLed::setAnimation(Animation* anim)
@@ -29,6 +29,253 @@ void GestureBoardHalLed::stop()
 
 void GestureBoardHalLed::reset()
 {
-  bool zeroMapping[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-  this->set(zeroMapping);
+  mappingLed zero;
+  zero.mappingLed_byte = 0;
+  this->setMapping(zero);
+  this->_count = 0;
+}
+
+uint16_t GestureBoardHalLed::getMapping()
+{
+  return this->_map.mappingLed_byte;
+}
+
+void GestureBoardHalLed::setMapping(mappingLed new_map)
+{
+  this->_map = new_map;
+}
+
+
+Animation* GestureBoardHalLed::getAnimation(int idx)
+{
+    return GestureBoardHalLed::tabAnimations[idx];
+}
+
+Animation* GestureBoardHalLed::tabAnimations[]={new SwipeLeft(), new SwipeRight(), new FiveButtons(), new ThreeButtons(),new TwoButtons(),new OneButton(),new SleepModePulse()};
+
+
+
+SwipeLeft::SwipeLeft()
+{
+}
+
+void SwipeLeft::update(GestureBoardHalLed& parent, int count)
+{
+  GestureBoardHalLed::mappingLed current_mapping;
+  current_mapping.mappingLed_byte = parent.getMapping();
+    switch (count)
+    {
+    case 1:
+      current_mapping.mappingLed_bit.ring1_0 = 1;
+      current_mapping.mappingLed_bit.ring1_60 = 1;
+      current_mapping.mappingLed_bit.ring1_120 = 1;
+      parent.setMapping(current_mapping);
+      break;
+    case 2:
+      current_mapping.mappingLed_bit.ring2_0 = 1;
+      current_mapping.mappingLed_bit.ring2_60 = 1;
+      current_mapping.mappingLed_bit.ring2_120 = 1;
+      parent.setMapping(current_mapping);
+      break;
+    case 3:
+      current_mapping.mappingLed_bit.ring3_0 = 1;
+      current_mapping.mappingLed_bit.ring3_60 = 1;
+      current_mapping.mappingLed_bit.ring3_120 = 1;
+      parent.setMapping(current_mapping);
+      break;
+    case 4:
+      current_mapping.mappingLed_bit.ring4_0 = 1;
+      current_mapping.mappingLed_bit.ring4_60 = 1;
+      current_mapping.mappingLed_bit.ring4_120 = 1;
+      parent.setMapping(current_mapping);
+      break;
+    case 5:
+      current_mapping.mappingLed_bit.ring5_0 = 1;
+      current_mapping.mappingLed_bit.ring5_60 = 1;
+      current_mapping.mappingLed_bit.ring5_120 = 1;
+      parent.setMapping(current_mapping);
+      break;
+    case 6:
+      current_mapping.mappingLed_byte = 0;
+      parent.setMapping(current_mapping);
+      parent.reset();
+      break;
+    }
+    parent.set();
+}
+
+SwipeRight::SwipeRight()
+{
+}
+
+void SwipeRight::update(GestureBoardHalLed& parent, int count)
+{
+  GestureBoardHalLed::mappingLed current_mapping;
+  current_mapping.mappingLed_byte = parent.getMapping();
+    switch (count)
+    {
+    case 1:
+      current_mapping.mappingLed_bit.ring5_0 = 1;
+      current_mapping.mappingLed_bit.ring5_60 = 1;
+      current_mapping.mappingLed_bit.ring5_120 = 1;
+      parent.setMapping(current_mapping);
+      break;
+    case 2:
+      current_mapping.mappingLed_bit.ring4_0 = 1;
+      current_mapping.mappingLed_bit.ring4_60 = 1;
+      current_mapping.mappingLed_bit.ring4_120 = 1;
+      parent.setMapping(current_mapping);
+      break;
+    case 3:
+      current_mapping.mappingLed_bit.ring3_0 = 1;
+      current_mapping.mappingLed_bit.ring3_60 = 1;
+      current_mapping.mappingLed_bit.ring3_120 = 1;
+      parent.setMapping(current_mapping);
+      break;
+    case 4:
+      current_mapping.mappingLed_bit.ring2_0 = 1;
+      current_mapping.mappingLed_bit.ring2_60 = 1;
+      current_mapping.mappingLed_bit.ring2_120 = 1;
+      parent.setMapping(current_mapping);
+      break;
+    case 5:
+      current_mapping.mappingLed_bit.ring1_0 = 1;
+      current_mapping.mappingLed_bit.ring1_60 = 1;
+      current_mapping.mappingLed_bit.ring1_120 = 1;
+      parent.setMapping(current_mapping);
+      break;
+    case 6:
+      current_mapping.mappingLed_byte = 0;
+      parent.setMapping(current_mapping);
+      parent.reset();
+      break;
+    }
+    parent.set();
+}
+
+FiveButtons::FiveButtons()
+{
+}
+
+void FiveButtons::update(GestureBoardHalLed& parent, int count)
+{
+  GestureBoardHalLed::mappingLed current_mapping;
+  current_mapping.mappingLed_byte = parent.getMapping();
+    switch (count)
+    {
+    case 1:
+        current_mapping.mappingLed_bit.ring1_0 = 1;
+        current_mapping.mappingLed_bit.ring2_0 = 1;
+        current_mapping.mappingLed_bit.ring3_0 = 1;
+        current_mapping.mappingLed_bit.ring4_0 = 1;
+        current_mapping.mappingLed_bit.ring5_0 = 1;
+        break;
+    case 2:
+        current_mapping.mappingLed_bit.ring1_60 = 1;
+        current_mapping.mappingLed_bit.ring2_60 = 1;
+        current_mapping.mappingLed_bit.ring3_60 = 1;
+        current_mapping.mappingLed_bit.ring4_60 = 1;
+        current_mapping.mappingLed_bit.ring5_60 = 1;
+        break;
+    case 3:
+        current_mapping.mappingLed_bit.ring1_120 = 1;
+        current_mapping.mappingLed_bit.ring2_120 = 1;
+        current_mapping.mappingLed_bit.ring3_120 = 1;
+        current_mapping.mappingLed_bit.ring4_120 = 1;
+        current_mapping.mappingLed_bit.ring5_120 = 1;
+        parent.reset();
+        break;
+    }
+    parent.set();
+}
+
+ThreeButtons::ThreeButtons()
+{
+}
+
+void ThreeButtons::update(GestureBoardHalLed& parent, int count)
+{
+  GestureBoardHalLed::mappingLed current_mapping;
+  current_mapping.mappingLed_byte = parent.getMapping();
+    switch (count)
+    {
+    case 1:
+        current_mapping.mappingLed_bit.ring1_0 = 1;
+        current_mapping.mappingLed_bit.ring3_0 = 1;
+        current_mapping.mappingLed_bit.ring5_0 = 1;
+        break;
+    case 2:
+        current_mapping.mappingLed_bit.ring1_60 = 1;
+        current_mapping.mappingLed_bit.ring3_60 = 1;
+        current_mapping.mappingLed_bit.ring5_60 = 1;
+        break;
+    case 3:
+        current_mapping.mappingLed_bit.ring1_120 = 1;
+        current_mapping.mappingLed_bit.ring3_120 = 1;
+        current_mapping.mappingLed_bit.ring5_120 = 1;
+        parent.reset();
+        break;
+    }
+    parent.set();
+}
+
+TwoButtons::TwoButtons()
+{
+
+}
+
+void TwoButtons::update(GestureBoardHalLed& parent, int count)
+{
+  GestureBoardHalLed::mappingLed current_mapping;
+  current_mapping.mappingLed_byte = parent.getMapping();
+    switch (count)
+    {
+    case 1:
+        current_mapping.mappingLed_bit.ring2_0 = 1;
+        current_mapping.mappingLed_bit.ring4_0 = 1;
+        break;
+    case 2:
+        current_mapping.mappingLed_bit.ring2_60 = 1;
+        current_mapping.mappingLed_bit.ring4_60 = 1;
+        break;
+    case 3:
+        current_mapping.mappingLed_bit.ring2_120 = 1;
+        current_mapping.mappingLed_bit.ring4_120 = 1;
+        parent.reset();
+        break;
+    }
+    parent.set();
+}
+
+OneButton::OneButton()
+{
+}
+
+void OneButton::update(GestureBoardHalLed& parent, int count)
+{
+  GestureBoardHalLed::mappingLed current_mapping;
+  current_mapping.mappingLed_byte = parent.getMapping();
+    switch (count)
+    {
+    case 1:
+        current_mapping.mappingLed_bit.ring3_0 = 1;
+        break;
+    case 2:
+        current_mapping.mappingLed_bit.ring3_60 = 1;
+        break;
+    case 3:
+        current_mapping.mappingLed_bit.ring3_120 = 1;
+        parent.reset();
+        break;
+    }
+    parent.set();
+}
+
+SleepModePulse::SleepModePulse()
+{
+}
+
+void SleepModePulse::update(GestureBoardHalLed& parent, int count)
+{
+
 }
--- projects/development_sketch/lib/gestureBoardHAL_LED/gestureBoardHAL_LED.h
+++ projects/development_sketch/lib/gestureBoardHAL_LED/gestureBoardHAL_LED.h
@@ -1,21 +1,107 @@
 #ifndef GESTURE_BOARD_HAL_LED_H
 #define GESTURE_BOARD_HAL_LED_H
 
+#include "Arduino.h"
 #include "animation.h"
+
+class GestureBoardHalLed;
+class Animation;
+
+class Animation
+{
+public:
+    virtual void update(GestureBoardHalLed& parent, int count) = 0;
+};
+
+class SwipeLeft : public Animation
+{
+  public:
+    SwipeLeft();
+    virtual void update(GestureBoardHalLed& parent, int count);
+};
+
+class SwipeRight : public Animation
+{
+  public:
+    SwipeRight();
+    virtual void update(GestureBoardHalLed& parent, int count);
+};
+
+class FiveButtons : public Animation
+{
+  public:
+    FiveButtons();
+    virtual void update(GestureBoardHalLed& parent, int count);
+};
+
+class ThreeButtons : public Animation
+{
+  public:
+    ThreeButtons();
+    virtual void update(GestureBoardHalLed& parent, int count);
+};
+
+class TwoButtons : public Animation
+{
+  public:
+    TwoButtons();
+    virtual void update(GestureBoardHalLed& parent, int count);
+};
+
+class OneButton : public Animation
+{
+  public:
+    OneButton();
+    virtual void update(GestureBoardHalLed& parent, int count);
+};
+
+class SleepModePulse : public Animation
+{
+  public:
+    SleepModePulse();
+    virtual void update(GestureBoardHalLed& parent, int count);
+};
+
 
 class GestureBoardHalLed
 {
-public:
-  GestureBoardHalLed(Animation* defaultAnim);
-  //virtual ~GestureBoardHAL();
-
   //class members
+  public:
+    struct mapping_bf {
+      uint16_t  ring3_0 : 1;
+      uint16_t  ring3_120 : 1;
+      uint16_t  ring3_60 : 1;
+      uint16_t  ring4_0 : 1;
+      uint16_t  ring4_120 : 1;
+      uint16_t  ring4_60 : 1;
+      uint16_t  ring5_0 : 1;
+      uint16_t  ring5_120 : 1;
+
+      uint16_t  n_a : 1;
+      uint16_t  ring2_60 : 1;
+      uint16_t  ring2_120 : 1;
+      uint16_t  ring2_0 : 1;
+      uint16_t  ring1_60 : 1;
+      uint16_t  ring1_120 : 1;
+      uint16_t  ring1_0 : 1;
+      uint16_t  ring5_60 : 1;
+    };
+
+    union mappingLed {
+      mapping_bf mappingLed_bit;
+      uint16_t mappingLed_byte;
+    };
 protected:
   Animation* _runningAnimation;
   bool _runningState;
   int _count;
+  mappingLed _map;
+//GestureBoardHAL methods
 public:
-  //GestureBoardHAL methods
+  GestureBoardHalLed(Animation* defaultAnim);
+  void setMapping(mappingLed new_map);
+  uint16_t getMapping();
+  //virtual ~GestureBoardHAL();
   void setAnimation(Animation* anim);
   void start();
   void stop();
@@ -23,8 +109,10 @@ public:
   //GestureBoardHAL virtual methods version dependent
   virtual void init() = 0;
   virtual void dim(int value) = 0;
-  virtual void set(bool mapping[]) = 0;
+  virtual void set() = 0;
   virtual void update() = 0;
+  static Animation* tabAnimations[];
+  static Animation* getAnimation(int idx);
 };
 
 #endif
