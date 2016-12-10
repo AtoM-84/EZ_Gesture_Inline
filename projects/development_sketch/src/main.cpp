3994b7dd72e6e9ec2ee216f163be8e7b7ff4e634
Arnaud PHELIPOT <aphelipot@gmail.com>
2016-12-10 14:45:46 +0100

    MAJ programme test port COM avec HAL COM.

--- projects/development_sketch/src/main.cpp
+++ projects/development_sketch/src/main.cpp
@@ -1,20 +1,27 @@
 #include "Arduino.h"
-#include "GestureBoardHAL_LED_V1_1.h"
 #include "GestureBoardHAL_V1_1.h"
+#include "GestureBoardHAL_COM_V1_1.h"
+#include "GestureBoardHAL_LED_V1_1.h"
 
 GestureBoardHal_V1_1* gestureBoardHAL = new GestureBoardHal_V1_1();
 GestureBoardHalLedV1_1 gestureBoardLED(GestureBoardHalLedV1_1::getAnimation(0), 500);
+GestureBoardHalCom_V1_1 gestureBoardCOM(9600);
 
 void setup()
 {
   gestureBoardHAL->init();
+  gestureBoardCOM.open();
 
   gestureBoardLED.setAnimation(GestureBoardHalLedV1_1::getAnimation(1));
   gestureBoardLED.init();
   gestureBoardLED.start();
+  //gestureBoardCOM._bufferOut = "toto\r\n";
 }
 
 void loop()
 {
   gestureBoardLED.update();
+  gestureBoardCOM.readMessage();
+  gestureBoardCOM.showNewMessage();
+  //gestureBoardCOM.sendMessage((char*)gestureBoardCOM._bufferOut, 6);
 }
