e354eb13f9c97ae7587fa7a097a3181808a129c4
Arnaud PHELIPOT <aphelipot@gmail.com>
2016-12-11 16:20:59 +0100

    Mise à jour programme test commande par port COM.

--- projects/development_sketch/src/main.cpp
+++ projects/development_sketch/src/main.cpp
@@ -4,24 +4,61 @@
 #include "GestureBoardHAL_LED_V1_1.h"
 
 GestureBoardHal_V1_1* gestureBoardHAL = new GestureBoardHal_V1_1();
-GestureBoardHalLedV1_1 gestureBoardLED(GestureBoardHalLedV1_1::getAnimation(0), 500);
+GestureBoardHalLedV1_1 gestureBoardLED(GestureBoardHalLedV1_1::getAnimation(0), 200);
 GestureBoardHalCom_V1_1 gestureBoardCOM(9600);
 
 void setup()
 {
   gestureBoardHAL->init();
   gestureBoardCOM.open();
-
-  gestureBoardLED.setAnimation(GestureBoardHalLedV1_1::getAnimation(1));
   gestureBoardLED.init();
-  gestureBoardLED.start();
+  gestureBoardLED.setAnimation(GestureBoardHalLedV1_1::getAnimation(2));
+    gestureBoardLED.start();
   //gestureBoardCOM._bufferOut = "toto\r\n";
 }
 
 void loop()
 {
   gestureBoardLED.update();
-  gestureBoardCOM.readMessage();
-  gestureBoardCOM.showNewMessage();
+    gestureBoardCOM.readMessage();
+    if(gestureBoardCOM._newMessage){
+      gestureBoardCOM.showLastMessage();
+      Serial.println(gestureBoardCOM._bufferIn[5]);
+      switch(gestureBoardCOM._bufferIn[5])
+      {
+        case 48:
+        gestureBoardLED.stop();
+        gestureBoardLED.setAnimation(GestureBoardHalLedV1_1::getAnimation(0));
+        gestureBoardLED.start();
+        break;
+        case 49:
+        gestureBoardLED.stop();
+        gestureBoardLED.setAnimation(GestureBoardHalLedV1_1::getAnimation(1));
+        gestureBoardLED.start();
+        break;
+        case 50:
+        gestureBoardLED.stop();
+        gestureBoardLED.setAnimation(GestureBoardHalLedV1_1::getAnimation(2));
+        gestureBoardLED.start();
+        break;
+        case 51:
+        gestureBoardLED.stop();
+        gestureBoardLED.setAnimation(GestureBoardHalLedV1_1::getAnimation(3));
+        gestureBoardLED.start();
+        break;
+        case 52:
+        gestureBoardLED.stop();
+        gestureBoardLED.setAnimation(GestureBoardHalLedV1_1::getAnimation(3));
+        gestureBoardLED.start();
+        break;
+        case 53:
+        gestureBoardLED.stop();
+        gestureBoardLED.setAnimation(GestureBoardHalLedV1_1::getAnimation(4));
+        gestureBoardLED.start();
+        break;
+      }
+
+  }
+  //gestureBoardCOM.showLastMessage();
   //gestureBoardCOM.sendMessage((char*)gestureBoardCOM._bufferOut, 6);
 }
