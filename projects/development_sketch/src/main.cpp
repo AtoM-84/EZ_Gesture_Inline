113a872ba11692c6eedc6123a3ce3f5595e685c2
Arnaud PHELIPOT <aphelipot@gmail.com>
2016-12-12 14:19:35 +0100

    Mise à jour main incluant test Range.

--- projects/development_sketch/src/main.cpp
+++ projects/development_sketch/src/main.cpp
@@ -2,63 +2,77 @@
 #include "GestureBoardHAL_ArduinoUNO_V1_1.h"
 #include "GestureBoardHAL_COM_V1_1.h"
 #include "GestureBoardHAL_LED_V1_1.h"
+#include "GestureBoardHAL_Range_V1_1.h"
 
 GestureBoardHal_ArduinoUNO_V1_1* gestureBoardHAL = new GestureBoardHal_ArduinoUNO_V1_1();
-GestureBoardHalLedV1_1 gestureBoardLED(GestureBoardHalLedV1_1::getAnimation(0), 200);
-GestureBoardHalCom_V1_1 gestureBoardCOM(9600);
+GestureBoardHalCom_V1_1* gestureBoardCOM = new GestureBoardHalCom_V1_1(115200);
+GestureBoardHalLed_V1_1* gestureBoardLED = new GestureBoardHalLed_V1_1(GestureBoardHalLed_V1_1::getAnimation(0), 150);
+GestureBoardHalRange_V1_1* gestureBoardRange = new GestureBoardHalRange_V1_1();
+
 
 void setup()
 {
   gestureBoardHAL->init();
-  gestureBoardCOM.open();
-  gestureBoardLED.init();
-  gestureBoardLED.setAnimation(GestureBoardHalLedV1_1::getAnimation(2));
-  gestureBoardLED.start();
+  gestureBoardCOM->open();
+  gestureBoardLED->init();
+  gestureBoardRange->init();
+  gestureBoardLED->setAnimation(GestureBoardHalLed_V1_1::getAnimation(2));
+  gestureBoardLED->start();
   //gestureBoardCOM._bufferOut = "toto\r\n";
 }
 
 void loop()
 {
-  gestureBoardLED.update();
-    gestureBoardCOM.readMessage();
-    if(gestureBoardCOM._newMessage){
-      gestureBoardCOM.showLastMessage();
-      Serial.println(gestureBoardCOM._bufferIn[5]);
-      switch(gestureBoardCOM._bufferIn[5])
+  gestureBoardLED->update();
+    gestureBoardCOM->readMessage();
+    if(gestureBoardCOM->_newMessage){
+      gestureBoardCOM->showLastMessage();
+      //Serial.println(gestureBoardCOM->_bufferIn[5]);
+    switch(gestureBoardCOM->_bufferIn[5])
       {
         case 48:
-        gestureBoardLED.stop();
-        gestureBoardLED.setAnimation(GestureBoardHalLedV1_1::getAnimation(0));
-        gestureBoardLED.start();
+        gestureBoardLED->stop();
+        gestureBoardLED->setAnimation(GestureBoardHalLed_V1_1::getAnimation(0));
+        gestureBoardLED->start();
         break;
         case 49:
-        gestureBoardLED.stop();
-        gestureBoardLED.setAnimation(GestureBoardHalLedV1_1::getAnimation(1));
-        gestureBoardLED.start();
+        gestureBoardLED->stop();
+        gestureBoardLED->setAnimation(GestureBoardHalLed_V1_1::getAnimation(1));
+        gestureBoardLED->start();
         break;
         case 50:
-        gestureBoardLED.stop();
-        gestureBoardLED.setAnimation(GestureBoardHalLedV1_1::getAnimation(2));
-        gestureBoardLED.start();
+        gestureBoardLED->stop();
+        gestureBoardLED->setAnimation(GestureBoardHalLed_V1_1::getAnimation(2));
+        gestureBoardLED->start();
         break;
         case 51:
-        gestureBoardLED.stop();
-        gestureBoardLED.setAnimation(GestureBoardHalLedV1_1::getAnimation(3));
-        gestureBoardLED.start();
+        gestureBoardLED->stop();
+        gestureBoardLED->setAnimation(GestureBoardHalLed_V1_1::getAnimation(3));
+        gestureBoardLED->start();
         break;
         case 52:
-        gestureBoardLED.stop();
-        gestureBoardLED.setAnimation(GestureBoardHalLedV1_1::getAnimation(3));
-        gestureBoardLED.start();
+        gestureBoardLED->stop();
+        gestureBoardLED->setAnimation(GestureBoardHalLed_V1_1::getAnimation(3));
+        gestureBoardLED->start();
         break;
         case 53:
-        gestureBoardLED.stop();
-        gestureBoardLED.setAnimation(GestureBoardHalLedV1_1::getAnimation(4));
-        gestureBoardLED.start();
+        gestureBoardLED->stop();
+        gestureBoardLED->setAnimation(GestureBoardHalLed_V1_1::getAnimation(4));
+        gestureBoardLED->start();
         break;
       }
 
   }
+  gestureBoardRange->rowMeasurement();
+  Serial.print(gestureBoardRange->_range1,DEC);
+  Serial.print(" | ");
+  Serial.print(gestureBoardRange->_range2,DEC);
+  Serial.print(" | ");
+  Serial.print(gestureBoardRange->_range3,DEC);
+  Serial.print(" | ");
+  Serial.print(gestureBoardRange->_range4,DEC);
+  Serial.print(" | ");
+  Serial.println(gestureBoardRange->_range5,DEC);
   //gestureBoardCOM.showLastMessage();
   //gestureBoardCOM.sendMessage((char*)gestureBoardCOM._bufferOut, 6);
 }
